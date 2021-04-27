db68133b0a6e49da5b75df744eaaf40b9ecfc310
Calle Svensson <calle.svensson@zeta-two.com>
2021-04-27 18:43:37 +0200

    Proper type size

--- service/src/encryptor.c
+++ service/src/encryptor.c
@@ -1,10 +1,11 @@
 #include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include "xxtea.h"
 
 
 #define NUM_BLOCKS 4
-#define PLAINTEXT_LEN (sizeof(long)*NUM_BLOCKS)
+#define PLAINTEXT_LEN (sizeof(uint64_t)*NUM_BLOCKS)
 unsigned char plaintext[PLAINTEXT_LEN] = {"HELLO JORDAN!"}; 
 
 long key[4] = { 0x66686235, 0x68726a33, 0x726c643a, 0x6c6e7033 };
@@ -13,7 +14,7 @@ int main() {
     btea((long *)plaintext, NUM_BLOCKS, key);
 
     printf("#include <stddef.h>\n");
-    printf("unsigned char encrypted[%d] = { ", PLAINTEXT_LEN);
+    printf("unsigned char encrypted[%lu] = { ", PLAINTEXT_LEN);
     for(size_t i = 0; i < PLAINTEXT_LEN; i++) {
         printf("%#02x", plaintext[i]);
         if((i+1)==PLAINTEXT_LEN) {
